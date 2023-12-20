5de30fb25f7258f70ed739e66b3320b28dad5acf
shifta-robel <robelbire16@gmail.com>
2023-12-20 11:21:37 +0300

    idiomatized with traits TryFrom and TryInto

--- src/configs.rs
+++ src/configs.rs
@@ -18,73 +18,6 @@ pub struct Config{
     pub log: LogConfig,
 }
 
-impl Config{
-    pub fn build() -> Result<Self, ConfigError> {
-        let contents = get_contents(CONFIG_FILE)?;
-        let val = contents.parse::<Value>().map_err(|e| ConfigError::FailedToParseConfig(e.to_string()))?;
-        let table = val.as_table().ok_or_else(|| ConfigError::FailedToParseConfig("Failed to parse config as a table".to_string()))?;
-        let vals = table.get("filter").ok_or_else(|| ConfigError::NoFiltersFound)?;
-        let vals = vals.as_array().ok_or_else(|| ConfigError::FailedToParseConfig("Failed to parse filters as an array".to_string()))?;
-
-        let interface = match table.get("device") {
-            Some(value) => {
-                if let Some(v) = value.get("interface"){
-                    match v.as_str() {
-                        Some(s) => Interface::Custom(s.to_string()),
-                        None => Err(ConfigError::FailedToParseAsString(value.clone()))?,
-                    }
-                }else {
-                    Interface::Lookup
-                }
-            },
-            None => Interface::Lookup,
-        };
-
-        let log = match table.get("log") {
-            Some(value) => {
-                let filter_level = match value.get("log-level") {
-                    Some(v) => v.as_str().ok_or(ConfigError::FailedToParseAsString(v.clone()))?,
-                    None => DEFAULT_LOG_LEVEL
-                };
-                let log_level = slog::Level::from_str(filter_level);
-                let log_level = match log_level {
-                    Ok(s) => Ok(Some(s)),
-                    Err(_) => {
-                        if filter_level == "off" {
-                            Ok(None)
-                        }else{
-                            Err(ConfigError::InvalidLogLevel(filter_level.to_string()))
-                        }
-                    }
-                }?;
-                    // .map_err(|_| ConfigError::InvalidLogLevel(String::from(filter_level)));
-                let log_file = match value.get("log-file") {
-                    Some(v) => {
-                        let st = v.as_str().ok_or(ConfigError::FailedToParseAsString(v.clone()))?;
-                        Some(
-                            OpenOptions::new().create(true).write(true).truncate(true).open(st).unwrap()
-                            )
-                        // Some(OsString::from(st))
-                    },
-                    None => None
-                };
-                LogConfig{
-                    log_level,
-                    log_file
-                }
-            },
-            None => LogConfig::default()
-        };
-        // let log = 
-        let mut vec: Vec<_> = Vec::with_capacity(vals.len());
-        for i in vals { vec.push(MidFilter::to_mid_filter(i)?); }
-        let mut filter: Vec<Filter> = Vec::with_capacity(vec.len());
-        for i in vec { filter.push(i.get_filter()?); }
-        filter.sort();
-        Ok(Self {filter, interface,log })
-    }
-}
-
 #[derive(Debug,Clone)]
 pub enum Interface {
     Lookup,
@@ -106,6 +39,80 @@ impl Default for LogConfig {
     }
 }
 
+impl Config{
+    pub fn build() -> Result<Self, ConfigError> {
+        let contents = get_contents(CONFIG_FILE)?;
+        let val = contents.parse::<Value>().map_err(|e| ConfigError::FailedToParseConfig(e.to_string()))?;
+        let table = val.as_table().ok_or_else(|| ConfigError::FailedToParseConfig("Failed to parse config as a table".to_string()))?;
+
+        let interface = get_interface(table)?;
+        let log = get_log(table)?;
+
+        let vals = table.get("filter").ok_or_else(|| ConfigError::NoFiltersFound)?;
+        let vals = vals.as_array().ok_or_else(|| ConfigError::FailedToParseConfig("Failed to parse filters as an array".to_string()))?;
+
+        let mut vec: Vec<_> = Vec::with_capacity(vals.len());
+        for i in vals { vec.push(MidFilter::try_from(i)?); }
+
+        let mut filter: Vec<Filter> = Vec::with_capacity(vec.len());
+        for i in vec { filter.push(i.try_into()?); }
+        filter.sort();
+
+        Ok(Self {filter, interface,log })
+    }
+}
+
+fn get_interface(table: &toml::map::Map<String, Value>) -> Result<Interface, ConfigError> {
+    match table.get("device") {
+            Some(value) => {
+                if let Some(v) = value.get("interface"){
+                    match v.as_str() {
+                        Some(s) => Ok(Interface::Custom(s.to_string())),
+                        None => Err(ConfigError::FailedToParseAsString(value.clone()))?,
+                    }
+                }else {
+                    Ok(Interface::Lookup)
+                }
+            },
+            None => Ok(Interface::Lookup),
+        }
+}
+
+fn get_log(table: &toml::map::Map<String, Value>) -> Result<LogConfig, ConfigError> {
+    match table.get("log") {
+        Some(value) => {
+            let filter_level = match value.get("log-level") {
+                Some(v) => v.as_str().ok_or(ConfigError::FailedToParseAsString(v.clone()))?,
+                None => DEFAULT_LOG_LEVEL
+            };
+            let log_level = slog::Level::from_str(filter_level);
+            let log_level = match log_level {
+                Ok(s) => Ok(Some(s)),
+                Err(_) => {
+                    if filter_level == "off" {
+                        Ok(None)
+                    }else{
+                        Err(ConfigError::InvalidLogLevel(filter_level.to_string()))
+                    }
+                }
+            }?;
+            let log_file = match value.get("log-file") {
+                Some(v) => {
+                    let st = v.as_str().ok_or(ConfigError::FailedToParseAsString(v.clone()))?;
+                    Some(
+                        OpenOptions::new().create(true).write(true).truncate(true).open(st).unwrap()
+                        )
+                },
+                None => None
+            };
+            Ok(LogConfig{
+                log_level,
+                log_file
+            })
+        },
+        None => Ok(LogConfig::default())
+    }
+}
 
 #[derive(Deserialize, Debug)]
 struct MidFilter{
@@ -118,34 +125,35 @@ struct MidFilter{
     kill: Option<bool>,
 }
 
-impl MidFilter {
-    fn to_mid_filter(item: &Value) -> Result<Self, ConfigError> {
-        let src = match item.get("src") {
-            Some(v) => Some(Self::ip_vec_from_value(v)?),
+impl TryFrom<&toml::Value> for MidFilter {
+    type Error = ConfigError;
+    fn try_from(value: &toml::Value) -> std::result::Result<Self, Self::Error> {
+        let src = match value.get("src") {
+            Some(v) => Some(ip_vec_from_value(v)?),
             None => None
         };
-        let dst = match item.get("dst") {
-            Some(v) => Some(Self::ip_vec_from_value(v)?),
+        let dst = match value.get("dst") {
+            Some(v) => Some(ip_vec_from_value(v)?),
             None => None
         };
-        let src_exclude = match item.get("src_exclude") {
-            Some(v) => Some(Self::ip_vec_from_value(v)?),
+        let src_exclude = match value.get("src_exclude") {
+            Some(v) => Some(ip_vec_from_value(v)?),
             None => None
         };
-        let dst_exclude = match item.get("dst_exclude") {
-            Some(v) => Some(Self::ip_vec_from_value(v)?),
+        let dst_exclude = match value.get("dst_exclude") {
+            Some(v) => Some(ip_vec_from_value(v)?),
             None => None
         };
-        let src_regex = match item.get("src_regex") {
-           Some(v) => Some(Self::string_from_value(v)?.to_string()),
+        let src_regex = match value.get("src_regex") {
+           Some(v) => Some(string_from_value(v)?.to_string()),
            None => None
         };
-        let dst_regex = match item.get("dst_regex") {
-           Some(v) => Some(Self::string_from_value(v)?.to_string()),
+        let dst_regex = match value.get("dst_regex") {
+           Some(v) => Some(string_from_value(v)?.to_string()),
            None => None
         };
-        let kill = match item.get("kill"){
-            Some(v) => Some(Self::bool_from_value(v)?),
+        let kill = match value.get("kill"){
+            Some(v) => Some(bool_from_value(v)?),
             None => None
         };
 
@@ -159,8 +167,11 @@ impl MidFilter {
             kill
         })
     }
+}
 
-    fn get_filter(&self) -> Result<Filter, ConfigError> {
+impl TryInto<Filter> for MidFilter {
+    type Error = ConfigError;
+    fn try_into(self) -> std::result::Result<Filter, Self::Error> {
         if self.src.is_some() && self.src_regex.is_some() ||
         self.src.is_some() && self.src_exclude.is_some() ||
         self.src_regex.is_some() && self.src_exclude.is_some() {
@@ -196,28 +207,28 @@ impl MidFilter {
         }
         if let Some(b) = self.kill { if !b {fil.kill = false} }
         Ok(fil)
-    }
-
-    fn ip_vec_from_value(item: &Value) -> Result<Vec<IpAddr>, ConfigError> {
-        let v= item.as_array().ok_or(ConfigError::ExpectedAList)?;
-        let mut vec: Vec<IpAddr> = Vec::with_capacity(v.len());
-        for i in v {
-            let s = i.as_str().ok_or(ConfigError::FailedToParseAsString(i.clone()))?;
-            let s = IpAddr::from_str(s).map_err(|e| ConfigError::FailedToParseAsIpAddr(format!("{} : {}",e ,s )))?;
-            vec.push(s)
-        }
-        Ok(vec)
-    }
-
-    fn bool_from_value(item: &Value) -> Result<bool, ConfigError> {
-        item.as_bool().ok_or(ConfigError::InvalidValueForKill)
-    }
-
-    fn string_from_value(item: &Value) -> Result<&str, ConfigError> {
-        item.as_str().ok_or(ConfigError::FailedToParseAsString(item.clone()))
+        
     }
 }
 
+fn ip_vec_from_value(item: &Value) -> Result<Vec<IpAddr>, ConfigError> {
+    let v= item.as_array().ok_or(ConfigError::ExpectedAList)?;
+    let mut vec: Vec<IpAddr> = Vec::with_capacity(v.len());
+    for i in v {
+        let s = i.as_str().ok_or(ConfigError::FailedToParseAsString(i.clone()))?;
+        let s = IpAddr::from_str(s).map_err(|e| ConfigError::FailedToParseAsIpAddr(format!("{} : {}",e ,s )))?;
+        vec.push(s)
+    }
+    Ok(vec)
+}
+
+fn bool_from_value(item: &Value) -> Result<bool, ConfigError> {
+    item.as_bool().ok_or(ConfigError::InvalidValueForKill)
+}
+
+fn string_from_value(item: &Value) -> Result<&str, ConfigError> {
+    item.as_str().ok_or(ConfigError::FailedToParseAsString(item.clone()))
+}
 
 fn get_contents(path: &str) -> Result<String, std::io::Error> {
     std::fs::read_to_string(path)
